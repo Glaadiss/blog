41c38ca82656e1eb8c232de37a24a31bffc6cfbd
Glaadiss <bartekgladys@gmail.com>
2018-10-07 14:06:30 +0200

    Add contact information

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Contact = () => (
+  <Layout>
+    <h1>Contact information</h1>
+    <h3>bartekgladys@gmail.com</h3>
+    <h3>+48 514 373 276</h3>
+  </Layout>
+)
+
+export default Contact
